2630f95e861e47366d1a144279748b32171af820
Nicola Selleri <nicola.selleri@fao.org>
2015-11-03 19:30:41 +0100

    Home and Style

--- scripts/index/host.js
+++ scripts/index/host.js
@@ -70,7 +70,7 @@ define([
     Host.prototype.initPercentageAnimations = function () {
 
         var c ={
-            color: '#f0e352',
+            color: '#f07f52',
             strokeWidth: 10,
             trailWidth: 9,
             trailColor: "rgba(255,255,255,0.2)",
